697d98c3b868d6a0b1463299efc3be3ba54d2983
thyme <thymejoe@gmail.com>
2021-04-19 02:06:20 +0800

    add bottom appbar style

--- app/src/main/java/com/thymleaf/music/ui/BottomAppActivity.kt
+++ app/src/main/java/com/thymleaf/music/ui/BottomAppActivity.kt
@@ -1,17 +1,22 @@
 package com.thymleaf.music.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.LinearInterpolator
 import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.thymleaf.music.R
 import com.thymleaf.music.base.BaseSimpleActivity
 import com.thymleaf.music.databinding.ActivityBottomAppBinding
+
 
 class BottomAppActivity : BaseSimpleActivity() {
 
     private lateinit var binding: ActivityBottomAppBinding
     private lateinit var bottomAppBar: BottomAppBar
+    private lateinit var fab: FloatingActionButton
 
 
     override fun setBindingView(): View {
@@ -20,7 +25,17 @@ class BottomAppActivity : BaseSimpleActivity() {
     }
 
     override fun initActivity(savedInstanceState: Bundle?) {
-        bottomAppBar = binding.bottomAppBar;
+        bottomAppBar = binding.bottomAppBar
+        fab = binding.fab
+        val operatingAnim: Animation = AnimationUtils.loadAnimation(this, R.anim.mtrl_extended_fab_state_list_animator)
+        val lin = LinearInterpolator()
+        operatingAnim.interpolator = lin
+        fab.startAnimation(operatingAnim)
+
+
+        val decor: View = window.decorView
+        decor.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+//        decor.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 
         bottomAppBar.setNavigationOnClickListener {
 
